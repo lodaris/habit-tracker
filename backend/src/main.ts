6a54c651853b7ed630409951209d3cf0e21ec115
Lodar1s <lodar1sssss@gmail.com>
2026-04-15 14:00:43 +0300

    feat: add API functions and fix - enable CORS on backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // глобальна валідація для всіх ендпоінтів
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,            // відкидає поля яких немає в DTO
     forbidNonWhitelisted: true, // повертає помилку якщо прийшло зайве поле
